34bdc019455078ea80de84f991dcbf6bb10f3538
FOXEST57 <foxest57@gmail.com>
2022-11-02 17:41:25 +0100

    sauvegarde

--- dropdown.js
+++ dropdown.js
@@ -1,56 +1,70 @@
 export default dropdown;
 
-function dropdown (title) {
+function dropdown (title) 
+{
+    const wrapper = `.filter-wrapper[data-ref="${title}"]`;
     function buildDropdown(title){   
         const div = document.createElement('div')
             div.innerHTML = `
-            <button class="navShearch${title} navShearchWrapper toggle" data-ref="${title}">
-                <div class="navSherchText">${title}</div> 
-                <i class="fa far-regular fa-chevron-down"></i> 
-            </button>
-            <div class="result navShearch${title} hidden" data-ref="${title}">
-            <div class="reglage">
-                <input type="text" value = "${title} " class="search-input"/>
-                <i class="fa far-regular fa-chevron-up"></i> 
-            <div/> 
-                <div class="list"> <div/> 
-                <div id="no-result" class="hidden">Désolé rien ne corresponds a votre recherche<div/>   
-
+            <div class="filter-wrapper" data-ref="${title}">
+                <button class="navShearch${title} navShearchWrapper toggle">
+                    <div class="navSherchText">${title}</div> 
+                    <i class="fa far-regular fa-chevron-down"></i> 
+                </button>
+                <div class="result navShearch${title} hidden">
+                    <div class="reglage close">
+                        <input type="text" placeholder = "${title} " class="search-input"> </input>
+                        <i class="fa far-regular fa-chevron-up close-button"></i> 
+                    </div>
+                    <div class="list"> </div>
+                    <div id="no-result" class="hidden">Désolé rien ne corresponds a votre recherche</div>   
+                </div>
             </div>
-    `; 
-    return div;
-}
+       `; 
+        return div;
+    }
 
     //ouvre et ferme le dropdown
-    function displayDropdown(){
+    function displayDropdown()
+    {
+
         // crée le Dropdown vide
         const ingDropdown = buildDropdown(title)
         document.querySelector(".filters").append(ingDropdown)
-        document.querySelector(`.toggle[data-ref="${title}"]`).addEventListener("click", function (e) {
-            const result = document.querySelector(`.result[data-ref="${title}"]`);
-            if (result.classList.contains("hidden")) {
-                result.classList.remove("hidden");
-                document.querySelector(`.toggle[data-ref="${title}"]`).classList.add("hidden");
-            }else {
-                result.classList.add("hidden");
-                // document.querySelector(`.toggle[data-ref="${title}"]`).classList.remove("hidden")
-            }
+        listenForOpening()
+        listenForClosing()
+    }
+
+    function listenForOpening()
+    {
+        document.querySelector(`${wrapper} .toggle`).addEventListener("click", function () {
+            const result = document.querySelector(` ${wrapper} .result`);
+            result.classList.remove("hidden");
+            document.querySelector(`${wrapper} .toggle`).classList.add("hidden");    
         });
     }
     
+    function listenForClosing()
+    {
+        document.querySelector(`${wrapper} .close-button `).addEventListener("click", function () {
+            const result = document.querySelector(` ${wrapper} .result`);
+            result.classList.add("hidden");
+            document.querySelector(`${wrapper} .toggle`).classList.remove("hidden");    
+        });
+    }
     function hydrate(items){
         items.forEach((item) => {
-             document.querySelector(`.result[data-ref="${title}"] .list`).innerHTML += `<div class="item">${item}<div>` ;
+             document.querySelector(`${wrapper} .result .list`).innerHTML += `<div class="item">${item}<div>` ;
         });    
     }
 
     function listenForInput()
     {
-        document.querySelector(`.result[data-ref="${title}"] .search-input`).addEventListener('input', (e) =>
+        document.querySelector(`${wrapper} .result .search-input`).addEventListener('input', (e) =>
         {
             const needle = e.target.value.toLowerCase();
           
-            let elements = document.querySelectorAll(`.result[data-ref="${title}"] .list .item`);
+            let elements = document.querySelectorAll(`${wrapper} .result .list .item`);
             
             elements.forEach(el =>
                 {
@@ -67,7 +81,7 @@ function dropdown (title) {
                     el.classList.add('hidden')
                 })
 
-            const visibleItems = document.querySelectorAll(`.result[data-ref="${title}"] .list .item:not(.hidden)`).length;
+            const visibleItems = document.querySelectorAll(`${wrapper} .result.list .item:not(.hidden)`).length;
             if (visibleItems == 0)
             {
                 document.querySelector('#no-result').classList.remove('hidden');
